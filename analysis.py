4d1e397ed9d2fcaa858734e96bee12e2a789137b
Ryan Cox <G00398251@gmit.ie>
2021-03-30 20:04:04 +0100

    Pandas Dataframe used

--- analysis.py
+++ analysis.py
@@ -12,7 +12,7 @@ setosa = []
 versicolor = []
 virginica = []
 
-def iris_species():
+def iris_species():  ## Might want to replace this with Pandas version of importing csv. 
     with open(irisFile) as f:
         csvReading = csv.reader(f, delimiter=",")  #https://realpython.com/python-csv/#what-is-a-csv-file
         
@@ -34,32 +34,21 @@ versicolorarray = np.array(versicolor, float)
 virginicaarray = np.array(virginica, float)
 
 
+# Analysing the data. 
+allspecies = np.concatenate((setosaarray, versicolorarray, virginicaarray))
 
-# Need to analyse the data.  Think about PANDAs.  May need to import data above using PANDAS instead. 
-
-
-
-# Setosa contents for plotting  There has to be a more efficient way to do this!   Can I index it directly from the plotting code?? Unable to do so so far.  
-# Will get it working first and then think about efficentcy afterwards.  Might be better to use pandas .loc
-setosa_sepals_lenght = [item[0] for item in setosaarray]  #https://stackoverflow.com/questions/29934201/using-every-first-element-in-a-multidimensional-array
-setosa_sepals_width = [item[1] for item in setosaarray]
-setosa_petals_lenght = [item[2] for item in setosaarray]
-setosa_petals_width =  [item[3] for item in setosaarray]
-
-versicolor_sepals_lenght = [item[0] for item in versicolorarray]  
-versicolor_sepals_width = [item[1] for item in versicolorarray]
-versicolor_petals_lenght = [item[2] for item in versicolorarray]
-versicolor_petals_width =  [item[3] for item in versicolorarray]
-
-virginica_sepals_lenght = [item[0] for item in virginicaarray]  
-virginica_sepals_width = [item[1] for item in virginicaarray]
-virginica_petals_lenght = [item[2] for item in virginicaarray]
-virginica_petals_width =  [item[3] for item in virginicaarray]
+# Use this code instead so I can index for plotting instead of the long winded code below. 
+setosa_df = pd.DataFrame (setosaarray, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
+versicolor_df = pd.DataFrame (versicolorarray, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
+virginica_df = pd.DataFrame (virginicaarray, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
+totaldf = pd.DataFrame (allspecies, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
+## May want to run stats for each species seperately & other stats
+print(totaldf.describe())
 
 
-# Plotting data 
-# Sepal lenght X width
-plt.scatter(setosa_sepals_lenght, setosa_sepals_width, color = "red")
-plt.scatter(versicolor_sepals_lenght, versicolor_sepals_width, color = "yellow" )
-plt.scatter(virginica_sepals_lenght, virginica_sepals_width, color = "green")
+ # Better plotting  # Indexing the Data Frame instead of creating many ugly variables shown above. 
+ # will be best to create a function for these plots rather than writing below out many times. 
+plt.scatter(setosa_df["Sepal Lenght"], setosa_df["Sepal Width"] , color = "red")
+plt.scatter(versicolor_df["Sepal Lenght"], versicolor_df["Sepal Width"], color = "yellow")
+plt.scatter(virginica_df["Sepal Lenght"], virginica_df["Sepal Width"], color = "green")
 plt.show()
