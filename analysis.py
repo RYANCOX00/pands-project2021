4b6ef51c26483ce8f12ef20d5e63525f3f55c0f3
Ryan Cox <G00398251@gmit.ie>
2021-03-31 20:38:40 +0100

    project update

--- analysis.py
+++ analysis.py
@@ -27,26 +27,33 @@ def iris_species():  ## Might want to replace this with Pandas version of import
                 features = [row[0], row[1], row[2], row[3]]
                 virginica.append(features)
 
+# Creating a function to create the data for each species as a pandas data frame. 
+def Dataframe_Function(species):
+    species_data = pd.DataFrame (species, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
+    return species_data
+
+
 iris_species() 
 
-setosaarray = np.array(setosa, float)  # converting the lists to numpy arrays
-versicolorarray = np.array(versicolor, float)
+setosaarray = np.array(setosa, float)  # converting the lists to numpy arrays  # this may be replaced later if I 
+versicolorarray = np.array(versicolor, float)  # read in the csv directly to pandas. 
 virginicaarray = np.array(virginica, float)
 
 
 # Analysing the data. 
 allspecies = np.concatenate((setosaarray, versicolorarray, virginicaarray))
 
-# Use this code instead so I can index for plotting instead of the long winded code below. 
-setosa_df = pd.DataFrame (setosaarray, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
-versicolor_df = pd.DataFrame (versicolorarray, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
-virginica_df = pd.DataFrame (virginicaarray, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
-totaldf = pd.DataFrame (allspecies, columns=["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width"])
+# running the defined function above for each species (and total) and saving the dataframe as a variable 
+setosa_df = Dataframe_Function(setosaarray)
+versicolor_df = Dataframe_Function(versicolorarray)
+virginica_df = Dataframe_Function(virginicaarray)
+total_df = Dataframe_Function(allspecies)
+
 ## May want to run stats for each species seperately & other stats
-print(totaldf.describe())
+print(total_df.describe())
 
 
- # Better plotting  # Indexing the Data Frame instead of creating many ugly variables shown above. 
+ # Better plotting  # Indexing the Data Frame instead of creating many variables. 
  # will be best to create a function for these plots rather than writing below out many times. 
 plt.scatter(setosa_df["Sepal Lenght"], setosa_df["Sepal Width"] , color = "red")
 plt.scatter(versicolor_df["Sepal Lenght"], versicolor_df["Sepal Width"], color = "yellow")
