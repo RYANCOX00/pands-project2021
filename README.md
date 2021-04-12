7eaf1f97eda9c40229d8107bcadcb7151125d23f
Ryan Cox <G00398251@gmit.ie>
2021-04-12 11:05:47 +0100

    writing to summary file bug fixed

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ In relation to opening the file, I am currently using the Python open file funct
 I created empty lists for the raw data can be extracted to.  I originally done this by the feature of the species but ran into trouble later when designing my pilot code (unable to seperate by species when plotting).   I then decided it would be best to extract by way of species themselves using a for loop and == "the species" eg. if row[4] == "Iris-setosa":.
 
 In relation to extracting the features of each species for plotting, later I originally extracted these into seperate lists for each species.  This essentially created 12 seperate list.  I was determined to create a more efficient way to extract this data for plotting.  I decided to create a PANDAS array for the 3 species and 1 for all of the species.  This made its easy to plot by way of indexing e.g. plt.scatter(setosa_df["Sepal Lenght"], setosa_df["Sepal Width"]).  Also the arrays including the array for total data made its easy to analyse using PANDAS functions.  
+
+
+File now read in through Pandas.  Some work was involved in grouping the species and then amneding variables throughout the code. 
+
+Also included is a check to see if there is any null/ missing data, and a output to inform the user that there is no missing data is this is the case. 
+
+Included is a statement to the user of the species of Iris flower.  I took the unique items from the column and stripped the "Iris-" and capitalized each species before printing as a one line for loop. 
+
+
+
+##### need to think about appending to the summary file.  Not all the data is going onto it.  Do I need to do it in one go, is only the last bit of content going to to it   -bug fixed with summary file.
+
+Need to think about a more effecient way to write to the file.   Maybe in one go via a list of strings. 
--- analysis.py
+++ analysis.py
@@ -9,8 +9,9 @@ import pandas as pd
 irisFile = "iris_csv.csv"
 summaryFile = "species_summary.txt"
 
-def writing_to_file(summary):
-    with open (summaryFile, "wt") as f:
+
+def writing_to_summary(summary):
+    with open (summaryFile, "at") as f:
         f.write(str(summary))
 
 
@@ -21,7 +22,9 @@ df.columns = ["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width","Spec
 #Checking for missing value
 null_values = df.isnull().values.any() #Delft Stack
 if null_values == False:
-    print("There is no missing values in this data set. \n")
+    print("\nThere is no missing values in this data set.") # optional for now, main purpose to the output to summary file.
+    no_null = "There is no missing values in this data set."
+    writing_to_summary(no_null)
 
 
 gp = df.groupby("Species")
@@ -30,29 +33,47 @@ versicolor = gp.get_group('Iris-versicolor')
 virginica = gp.get_group('Iris-virginica')
 
 
-species = (df['Species'].unique())
+# Find unique species and saving as a list. Manipulating the list to drop the "Iris-" and capitalize each species. 
+# Outputting to the user the type of Iris species added to a string. 
+species = (df['Species'].unique()) 
 species = [i.replace("Iris-", "").capitalize() for i in species]
-the_species = print ("The species of Irish flower are being analysed are: ", *species, sep = "\n")
+text = "\n\nThe species of Iris flower are being analysed are: "
+species1 = ", ".join(map(str, species)) # joining the contents of the list as a string with ", " as the seperator. 
+species_output = (text + species1)
+writing_to_summary(species_output)
+print(species_output)
 
 
-print("\n")
 
 
-## May want to run stats for each species seperately & other stats # bar chart for the total sample of each i.e. 50/50/50?
-print("Full summary has been written to text file. \n")
-full_summary = df.describe() # May not need to print. rather output to text file 
-writing_to_file(full_summary)
+## May want to run other stats # bar chart for the total sample of each i.e. 50/50/50?
+
+full_summary_text = ("\n\nFull summary of all the Iris species:\n")
+full_summary = df.describe() 
+writing_to_summary(full_summary_text)
+writing_to_summary(full_summary)
+print("\nFull summary has been written to text file.")
 
 
-print("A summary of the Setosa species:")
-print(setosa.describe())
+setosa_summary_text = ("\n\nA summary of the Setosa species:\n") 
+setosa_summary = setosa.describe()
+writing_to_summary(setosa_summary_text)
+writing_to_summary(setosa_summary)
+print("\nSetosa summary has been written to text file.")
 
-print("\nA summary of the Versicolor species")
-print(versicolor.describe())
 
-print("\nA summary of the Virginica species")
-print(virginica.describe())
+versicolor_summary_text = ("\n\nA summary of the Versicolor species:\n") 
+versicolor_summary = versicolor.describe()
+writing_to_summary(versicolor_summary_text)
+writing_to_summary(versicolor_summary)
+print("\nVersicolor summary has been written to text file.")
 
+
+virginica_summary_text = ("\n\nA summary of the Virginica species:\n") 
+virginica_summary = virginica.describe()
+writing_to_summary(virginica_summary_text)
+writing_to_summary(virginica_summary)
+print("\nVirginica summary has been written to text file.")
 
 
 ## Better plotting  # Indexing the Data Frame instead of creating many variables. 
